c115ef17d78c847b687e1e99859a5bf99d02812d
Mads Mansour Larsen <42993941+madsmansour@users.noreply.github.com>
2020-03-31 14:02:15 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import YouTube from 'react-youtube';
+import { AwesomeButton } from "react-awesome-button";
+import "react-awesome-button/dist/styles.css";
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Example/>
+        <AwesomeButton/>
       </header>
     </div>
   );
 }
+
+class Example extends React.Component {
+  render() {
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: { // https://developers.google.com/youtube/player_parameters
+        autoplay: 1
+      }
+    };
+
+    return (
+      <YouTube
+        videoId="dQw4w9WgXcQ"
+        opts={opts}
+        onReady={this._onReady}
+      />
+    );
+  }
+
+  _onReady(event) {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  }
+}
+
+function Button() {
+  return <AwesomeButton type="primary">Button</AwesomeButton>;
+}
+
+
 
 export default App;
